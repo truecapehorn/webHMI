1ec07af2daf054f88b9d3557ff49779a4e94cc70
truecapehorn <truecapehorn@gmail.com>
2019-01-19 12:06:51 +0100

    wyjatek jak uzytkownik wprowadzi zle date

--- graph_range.py
+++ graph_range.py
@@ -8,12 +8,16 @@ def range():
     wh_slices = 400
 
     print('Pobierz dane dla dnia: rrrr-mm-dd: ',end='>> ')
-    wh_start = input()
-    wh_start=[int(i) for i in wh_start.split('-')]
-    dt = datetime(wh_start[0], wh_start[1], wh_start[2], 0, 0)
-    unixtime = time.mktime(dt.timetuple())
-    date=datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d')
-
+    while True:
+        try:
+            wh_start = input()
+            wh_start = [int(i) for i in wh_start.split('-')]
+            dt = datetime(wh_start[0], wh_start[1], wh_start[2], 0, 0)
+            unixtime = time.mktime(dt.timetuple())
+            date = datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d')
+            break
+        except :
+            print('Zła date!!\nWprowadz jescze raz date rrrr-mm-dd: ',end='>> ')
     return int(unixtime), wh_slices,date
 
 
