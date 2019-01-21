b84f1b01f75498df6ada82c29db82c815362363d
truecapehorn <truecapehorn@gmail.com>
2019-01-21 13:01:33 +0100

    usuniecie pustych linii w pliku scv. zmina sposobu wskazywania sciwski na wykorzystujacy os.sep

--- graphs_save.py
+++ graphs_save.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 import os
+import sys
 import re
 import graphs_data
 import csv
 
+sep=os.sep
 #todo: sworzyc wersje na linuxa i windowsa
 #todo: to jescze nie to :(
 
 
-def csv_writer(day,k,headers,rows):
-    with open('logi/dane_{}/graphs_{}.csv'.format(day,k), 'w') as f:
-        f_csv = csv.DictWriter(f, headers)
+def csv_writer(path,headers,rows):
+    with open(path, 'w') as f:
+        f_csv = csv.DictWriter(f, headers,lineterminator='\n')
         f_csv.writeheader()
         f_csv.writerows(rows)
 
@@ -19,29 +21,28 @@ def csv_writer(day,k,headers,rows):
 # Podział na pliki
 def save_data(unixtime,graphDatas):
     day = datetime.utcfromtimestamp(unixtime).strftime('%Y-%m-%d')
+    log_dir='logi{0}dane_{1}'.format(sep,day)
+
     try:
-        os.mkdir('logi/')
-    except FileExistsError:
-        pass
-    try:
-        os.mkdir('logi/dane_{}'.format(day))
+        os.makedirs(log_dir)
     except FileExistsError:
         pass
 
     for key, val in graphDatas.items():
-        print('Zapis danych dla Mieszknia {}'.format(k))
+        file_path='{}{}wykres_{}.csv'.format(log_dir,sep,key)
+        print('Zapis danych dla Mieszknia {}'.format(key))
         try:
-            os.remove('logi/dane_{}/graphs_{}.csv'.format(day,k))
+            os.remove(file_path)
         except FileNotFoundError:
             pass
         headers=[x.strip() for x in val[0].keys()]
-        csv_writer(day,key,headers,val)
+        csv_writer(file_path,headers,val)
 
 
 if __name__=='__main__':
     wh_start = 1547078400
     wh_slices = 4
-    print('Pobranie wykresow w dniu {} ilość próbek {}'.format(wh_start, wh_slices))
+    # print('Pobranie wykresow w dniu {} ilość próbek {}'.format(wh_start, wh_slices))
 
     graphDatas = graphs_data.datas(wh_start, wh_slices)
 
