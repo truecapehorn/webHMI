7d6c1d4aa9ff53a506082bc1674555d6ece41291
truecapehorn <truecapehorn@gmail.com>
2019-02-06 10:53:24 +0100

    cos tam chcialem zrobic zeby mi pokazalo jakie rejestry sa przypsiane dfo jakich polaczen

--- registers.py
+++ registers.py
@@ -27,8 +27,10 @@ for r in registers:
 
 if __name__ == '__main__':
 
-    file_path = 'rejestry/reg.csv'
-    file_path=file_path.replace('/', os.sep)
+    file_path1 = 'rejestry/registers.csv'
+    file_path2 = 'rejestry/regList.csv'
+    file_path1=file_path1.replace('/', os.sep)
+    file_path2 = file_path2.replace('/', os.sep)
 
     try:
         os.makedirs('rejestry')
@@ -37,7 +39,12 @@ if __name__ == '__main__':
 
     print('Ilosc  rejestrów : {}'.format(len(regList)))
 
-    for i in registers[0:10]:
-        print(i)
+    csv_writer(file_path1, registers)
 
-    csv_writer(file_path, registers)
+
+    regs=['104.4',105.5,106.6,205.5,206.6,204.4,207.7,304.4,305.5,306.6,404.4,405.5,402.2,403.3,406.6,504.4,505.5,506.6,604.4,605.5,602.2,603.3,606.6,710.10,703.3,705.5,709.9,708.8,707.7,704.4]
+
+    for k,v in regList.items():
+        # print(k,v)
+        if v['plcname'] in regs:
+            print(v['plcname'])
