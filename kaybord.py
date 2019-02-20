262d7fe973f937c5e7f37144c9b159dd3306a98d
truecapehorn <truecapehorn@gmail.com>
2019-02-20 14:18:51 +0100

    prace nad obejsciem wpisaywania danych

--- kaybord.py
+++ kaybord.py
@@ -1,10 +1,40 @@
-import sys, select
+import time
+from threading import Thread
 
-print ("You have ten seconds to answer!")
+answer = None
+t = 5  # You can type for 5 seconds
 
-i, o, e = select.select( [sys.stdin], [], [], 10 )
+def check():
+    time.sleep(5)
+    if answer != None:
+        print('ok wykonuj program 1',answer)
+        return
+    print("Too Slow")
 
-if (i):
-  print("You said", sys.stdin.readline().strip())
-else:
-  print ("You said nothing!")
+Thread(target=check).start()
+
+answer = input("Input something: ")
+
+
+
+# from datetime import datetime, timedelta
+# answer = None
+# t = 5  # You can type for 5 seconds
+# def timeup():
+#     final_time = datetime.now() + timedelta(seconds=t)
+#     print("You can enter now for" + str(t) + " seconds")
+#     while datetime.now() < final_time:
+#         print(datetime.now())
+#         answer = input()
+#         if datetime.now() > final_time:
+#             if answer != None:
+#                 print('ok wykonuj program 1',answer)
+#                 break
+#             else:
+#                 print('dupa')
+#                 break
+#
+#     print("STOP TYPING")
+#
+# timeup()
+
